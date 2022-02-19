df322e1bd54291f8b473508241b2ab808f8af81c
DESKTOP-PTH34BJ\User <konlawich.kade@bumail.net>
2022-02-19 11:13:19 +0700

    work06ex01

---
+++ src/NoobSAO.js
@@ -0,0 +1,13 @@
+import './css/NoobSAO.css';
+
+function NoobSAO(){
+    return (
+        <div>
+            <div className="ball">A</div>
+            <div className="ball">B</div>
+            <div className="ball">C</div>
+        </div>
+    )
+}
+
+export default NoobSAO;
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 import App from './App.js';
 import Profile from './Profile.js';
+import NoobSAO from './NoobSAO.js';
 
 
 function Router() {
@@ -9,6 +10,7 @@ function Router() {
             <Routes>
                 <Route path="/" element={ <App/>} />
                 <Route path="/profile" element={ <Profile/> } />
+                <Route path="/NoobSAO" element={ <NoobSAO/> } />
             </Routes>
         </BrowserRouter>
 
